5a25c7403f2d4697f8fe24b7a56acca0586073f3
Ted Nguyen <tpnguyen1@scu.edu>
2019-08-09 13:00:59 -0700

    Arduino file to output sample data to Serial monitor

---
+++ sketch/sketch.ino
@@ -0,0 +1,65 @@
+/*************************************
+ * For Arduino UNO OLED Displays: SDA -> A5, SCL -> A4
+ * Address is 0x3C
+ */
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+ 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show the display
+  display.display();
+  delay(2000); // Wait 2 seconds
+  
+  // Clear the display
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+  int x = 0;
+
+  // Clear the display and reset the cursor
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  
+  // Prints the numbers 0 through 10
+  while(x <= 9) {
+    Serial.print(x);
+    Serial.print(", ");
+    
+    display.print(x);
+    display.print(", ");
+    display.display(); // update display
+    
+    delay(500);
+    x++; 
+  }
+
+  Serial.print(x);
+  
+  display.print(x);
+  display.display();
+  
+  delay(2000); // Wait 2 seconds
+  // Stops the loop
+  while(1);
+}
